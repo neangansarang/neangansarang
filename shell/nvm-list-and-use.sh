4bd9c3dcb746f58e9cd2f93f81013d2f4a7cf8a8
neangansarang <neangansarangkamarang@gmail.com>
2026-05-21 21:01:05 +0700

    feat: add nvm list, use and current check version

---
+++ shell/nvm-list-and-use.sh
@@ -0,0 +1,26 @@
+~$ nvm list
+->     v24.14.0
+default -> 24 (-> v24.14.0 *)
+iojs -> N/A (default)
+node -> stable (-> v24.14.0 *) (default)
+stable -> 24.14 (-> v24.14.0 *) (default)
+unstable -> N/A (default)
+lts/argon -> v4.9.1 (-> N/A)
+lts/boron -> v6.17.1 (-> N/A)
+lts/carbon -> v8.17.0 (-> N/A)
+lts/dubnium -> v10.24.1 (-> N/A)
+lts/erbium -> v12.22.12 (-> N/A)
+lts/fermium -> v14.21.3 (-> N/A)
+lts/gallium -> v16.20.2 (-> N/A)
+lts/hydrogen -> v18.20.8 (-> N/A)
+lts/iron -> v20.20.0 (-> N/A)
+lts/jod -> v22.22.0 (-> N/A)
+lts/krypton -> v24.14.0 *
+lts/* -> lts/krypton (-> v24.14.0 *)
+
+~$ nvm use 20.19
+v20.19.6 is already installed.
+Now using node v20.19.6 (npm v10.8.2)
+
+~$ nvm current
+v20.19.6
